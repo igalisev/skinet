7776fdfa4c0283b327aca8d83e3fe5eef8c12be7
Ivan <igall@iname.com>
2023-10-17 16:34:37 +0300

    Add project files.

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+
+        }
+
+        //public string DbPath { get; }
+
+        //public StoreContext()
+        //{
+        //    var folder = Environment.SpecialFolder.LocalApplicationData;
+        //    var path = Environment.GetFolderPath(folder);
+        //    DbPath = System.IO.Path.Join(path, "store.db");
+        //    //DbPath = "store.db";
+        //}
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        //protected override void OnConfiguring(DbContextOptionsBuilder options)
+        //    => options.UseSqlite($"Data Source={DbPath}");
+
+    }
+}
